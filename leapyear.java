c379b821060aea066063fbd95b91030331e9117b
Sabari66 <sabaris.23ece@kongu.edu>
2026-04-29 11:44:33 +0530

    day 3

---
+++ leapyear.java
@@ -0,0 +1,15 @@
+class leapyear {
+    public static void main(String[] args) {
+        int year = 2024;
+
+        if (year % 400 == 0) {
+            System.out.println("Leap Year");
+        } else if (year % 100 == 0) {
+            System.out.println("Not a Leap Year");
+        } else if (year % 4 == 0) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+    }
+}
