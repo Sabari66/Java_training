4221232058b009a07090939bc5319e79f3504646
Sabari66 <sabaris.23ece@kongu.edu>
2026-04-29 14:37:23 +0530

    day 3
    Signed-off-by: Sabari66 <sabaris.23ece@kongu.edu>

---
+++ user.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class user {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 1;
+
+        while (num!=0) {
+            num = sc.nextInt();
+            System.out.println("You entered: " + num);
+        }
+    }
+}
