a81aaf7d40367202b1a4b253a263148b4c52fe4f
Sabari66 <sabaris.23ece@kongu.edu>
2026-05-01 11:28:38 +0530

    DAY 5
    Signed-off-by: Sabari66 <sabaris.23ece@kongu.edu>

---
+++ emptyblank.java
@@ -0,0 +1,9 @@
+class emptyblank {
+    public static void main(String[] args) {
+        String s1 = "";
+        String s2 = "   ";
+
+        System.out.println(s1.isEmpty());
+        System.out.println(s2.isBlank());
+    }
+}
