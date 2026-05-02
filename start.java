8ac2bdd9d0c98f91c902ce228e0b8c395a5c99d5
Sabari66 <sabaris.23ece@kongu.edu>
2026-05-02 14:22:40 +0530

    DAT 6
    Signed-off-by: Sabari66 <sabaris.23ece@kongu.edu>

---
+++ start.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class start {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        if (s.startsWith("Java"))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
